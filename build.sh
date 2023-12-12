213caf4785c286a43f4b3d9fc3ff72ca993fd037
Jan-E <github@ehrhardt.nl>
2023-12-12 08:56:25 +0100

    Add lipo -info to build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 ./iSSH2.sh --platform=iphoneos --min-version=8.0 --archs="arm64 arm64e x86_64"
+lipo -info ./libssh2_iphoneos/lib/libssh2.a
+lipo -info ./openssl_iphoneos/lib/libcrypto.a
+lipo -info ./openssl_iphoneos/lib/libssl.a
